2ae4a5fc460460d8c885cb67d2e92e12cace9a4a
Szymon Misztal <technosudo@softron.pl>
2023-10-22 23:29:09 +0200

    icons + fixes

--- app/src/main/java/com/technosudo/gymfollower/app/AppKoin.kt
+++ app/src/main/java/com/technosudo/gymfollower/app/AppKoin.kt
@@ -1,9 +1,11 @@
 package com.technosudo.gymfollower.app
 
 import android.content.Context
+import com.technosudo.gymfollower.ui.screens.main.MainViewModel
 import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
@@ -13,14 +15,26 @@ object AppKoin {
         single { AppDatabase.create(androidApplication()) }
     }
 
+    private val dataSources = module {
+    }
+
+    private val repositories = module {
+    }
+
     private val viewModels = module {
-        //viewModelOf(::)
+        viewModelOf(::MainViewModel)
+    }
+
+    private val misc = module {
     }
 
     private val modules by lazy {
         listOf(
             environments,
-            viewModels
+            dataSources,
+            repositories,
+            viewModels,
+            misc
         )
     }
 
